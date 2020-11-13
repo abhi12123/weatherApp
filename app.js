2e758e8f27856109729d4e600cd5133ea9f554e1
abhi12123 <vpofficial55@gmail.com>
2020-11-14 02:54:38 +0530

    weather app

---
+++ app.js
@@ -0,0 +1,38 @@
+window.addEventListener('load',()=>{
+    let long;
+    let lat;
+    let temperatureDescription=document.querySelector('.temperature-description');
+    let temperatureDegree=document.querySelector('.temperature-degree');
+    let locationTimezone=document.querySelector('.location-timezone');
+    let icon=document.querySelector('p');
+    let CorF=document.querySelector('span');
+    let degreeSection=document.querySelector('.degree-section');
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position=>{
+            long= position.coords.longitude;
+            lat=position.coords.latitude;
+            const proxy=`https://cors-anywhere.herokuapp.com/`;
+            const api=`http://api.weatherapi.com/v1/current.json?key=b481e56ae47e41628e6200454201311&q=${lat},${long}`;
+            fetch(api).then(Response=>{
+                return Response.json();
+            }).then(data =>{
+                console.log(data);
+                console.log(data.current.condition.icon);
+                temperatureDegree.textContent=data.current.temp_c;
+                locationTimezone.textContent=data.location.tz_id;
+                temperatureDescription.textContent=data.current.condition.text;
+                let imglink=data.current.condition.icon;
+                icon.innerHTML=`<img src='https:${imglink}'>`;
+                degreeSection.addEventListener('click',()=>{
+                    if (CorF.textContent=='C'){
+                        CorF.textContent='F';
+                        temperatureDegree.textContent=data.current.temp_f;
+                    }else{
+                        CorF.textContent='C';
+                        temperatureDegree.textContent=data.current.temp_c;
+                    }
+                })
+            })
+        });
+    }
+})
